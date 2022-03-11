ea109c6bf7beece8995859019a874e637475f685
Kyle Carberry <kyle@carberry.com>
2022-03-10 23:38:36 -0600

    Check double close

--- cmd/cloudflared/tunnel/signal.go
+++ cmd/cloudflared/tunnel/signal.go
@@ -10,6 +10,10 @@ import (
 
 // waitForSignal closes graceShutdownC to indicate that we should start graceful shutdown sequence
 func waitForSignal(graceShutdownC chan struct{}, logger *zerolog.Logger) {
+	if graceShutdownC == nil {
+		return
+	}
+
 	signals := make(chan os.Signal, 10)
 	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
 	defer signal.Stop(signals)
@@ -17,7 +21,11 @@ func waitForSignal(graceShutdownC chan struct{}, logger *zerolog.Logger) {
 	select {
 	case s := <-signals:
 		logger.Info().Msgf("Initiating graceful shutdown due to signal %s ...", s)
-		close(graceShutdownC)
+		select {
+		case <-graceShutdownC:
+		default:
+			close(graceShutdownC)
+		}
 	case <-graceShutdownC:
 	}
 }
